662dc91d551d3c0e9f117985e6a68c5684b2f61f
Luis Mendoza <luismendoza@Luiss-MacBook-Pro.local>
2017-10-25 17:26:54 -0700

    changed it to keyup function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,160 +4,88 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wins = 0;
-var losses = 0;
+var losses=0;
 var guessesLeft = 9;
 var currentGuesses=[];
-var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-console.log(randomLetter);
+var randomletter = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 
-function validate(guess){
-	if (currentGuesses.indexOf(guess) != -1 ) {
-		return guess
-	}
-	else {
-		alert("already pressed");
-	}
-}
-
-
-function rand(){
-	 alphabet[Math.floor(Math.random() * alphabet.length)];
-}
-
-
-// Function for guessed letter
-function guessedLetter() {
-	validate(guess);
-	var guess = document.getElementById("guess").value;
-	console.log(guess);
-	document.getElementById("guess").value="";
-
-
-	//Looping until we hit 9 times
-	if ( guessesLeft >= 1 ) {
-
-			if (randomLetter === guess){
-				wins++;
-				currentGuesses=[""]
-				guessesLeft=9;
-				console.log(guessesLeft);
-				rand();
-				console.log
-// Variables
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var currentGuesses=[];
-var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-
-console.log(randomLetter);
+console.log(randomletter);
 
 
 
+document.onkeyup = function (event) {
+	var guess = event.key;
+	document.getElementById("letter").innerHTML = guess;
 
+	
+	if (validate(guess) && newLetter(guess) ) {
 
-function rand(){
-	 alphabet[Math.floor(Math.random() * alphabet.length)];
-}
-
-
-// Function for guessed letter
-function guessedLetter() {
-
-	var guess = document.getElementById("guess").value;
-	console.log(guess);
-	document.getElementById("guess").value="";
-
-//Checking for duplicate letters
-if (currentGuesses.indexOf(guess) === -1 && guess.length === 1) {
-
-		//Runs while number of guesses are greater than 9
-		if ( guessesLeft >= 1 ) {
-
-				//
-				if (randomLetter === guess){
-					guessesLeft=9;
-					currentGuesses=[];
-					wins++;
-					console.log(guessesLeft);
-					rand();
-					console.log(randomLetter);
-					document.getElementById("wins").innerHTML="Wins: " + wins;
-					document.getElementById("losses").innerHTML="Losses: " + losses;
-					document.getElementById("guessesLeft").innerHTML="Guesses Left: " + guessesLeft;
-					document.getElementById("currentGuesses").innerHTML="Your Guesses so far: " + currentGuesses;			
-					// return
-				}
-				else {
-					guessesLeft--;
-					currentGuesses.push(guess);
-					// console.log(currentGuesses);
-					document.getElementById("wins").innerHTML="Wins: " + wins;
-					document.getElementById("losses").innerHTML="Losses: " + losses;
-					document.getElementById("guessesLeft").innerHTML="Guesses Left: " + guessesLeft;
-					document.getElementById("currentGuesses").innerHTML="Your Guesses so far: " + currentGuesses;
-					// return
-				} 
+		if (randomletter == guess ) {
+			wins++;
+			guessesLeft=9;
+			currentGuesses=[];			
+			document.getElementById("wins").innerHTML = wins;
+			randomletter = alphabet[Math.floor(Math.random() * alphabet.length)];
+			console.log(randomletter);
+		}
+		else if (guessesLeft > 0){
+			guessesLeft--;
+			currentGuesses.push(guess);
+			document.getElementById("guessesLeft").innerHTML = guessesLeft;
+			document.getElementById("currentGuesses").innerHTML= currentGuesses.join(" ");
 		}
 		else {
-			guessesLeft=9;
 			losses++;
-			currentGuesses= [];
-			document.getElementById("wins").innerHTML="Wins: " + wins;
-			document.getElementById("losses").innerHTML="Losses: " + losses;
-			document.getElementById("guessesLeft").innerHTML="Guesses Left: " + guessesLeft;
-			rand();
-			document.getElementById("currentGuesses").innerHTML="Your Guesses so far: " + currentGuesses;		
+			guessesLeft=9;
+			currentGuesses=[];
+			document.getElementById("guessesLeft").innerHTML = guessesLeft;
+			document.getElementById("losses").innerHTML = losses;
+			document.getElementById("currentGuesses").innerHTML= currentGuesses.join(" ");
+			randomletter = alphabet[Math.floor(Math.random() * alphabet.length)];
+			console.log(randomletter);
 		}
 
-
-}
-else {
-	alert("Repeaded Letter or entered a more than one letter");
-}
-
-}
-
-
-		
-(randomLetter);
-				document.getElementById("wins").innerHTML="Wins: " + wins;
-				document.getElementById("losses").innerHTML="Losses: " + losses;
-				document.getElementById("guessesLeft").innerHTML="Guesses Left: " + guessesLeft;
-				document.getElementById("currentGuesses").innerHTML="Your Guesses so far: " + currentGuesses;			
-				return
-			}
-			else {
-				guessesLeft--;
-				currentGuesses.push(guess);
-				// console.log(currentGuesses);
-				document.getElementById("wins").innerHTML="Wins: " + wins;
-				document.getElementById("losses").innerHTML="Losses: " + losses;
-				document.getElementById("guessesLeft").innerHTML="Guesses Left: " + guessesLeft;
-				document.getElementById("currentGuesses").innerHTML="Your Guesses so far: " + currentGuesses;
-				return
-			} 
+	}
+	else if (validate(guess) === false){
+		alert("invalid input");
 	}
 	else {
-		guessesLeft=9;
-		losses++;
-		document.getElementById("wins").innerHTML="Wins: " + wins;
-		document.getElementById("losses").innerHTML="Losses: " + losses;
-		document.getElementById("guessesLeft").innerHTML="Guesses Left: " + guessesLeft;
-		currentGuesses= [""]
-		rand();
-		document.getElementById("currentGuesses").innerHTML="Your Guesses so far: " + currentGuesses;		
+		alert("duplicated letter");
+
+		
 	}
 
+}
 
 
+
+
+
+//validating the input is a valid value
+function validate(input) {
+	var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    if( letters.indexOf(input) === -1 ) {
+    	return false;
+    }
+    return true;
 
 }
+
+function newLetter(letter) {
+	if ( currentGuesses.indexOf(letter) > -1  ) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+
+
 
 
 		
